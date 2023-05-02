a4158fc0938c9e5ca102dc7f3f34192c0bab8279
phapsucongu <hatiendong2004@gmail.com>
2023-05-03 02:34:28 +0700

    sap xep

---
+++ code/menu.h
@@ -0,0 +1,16 @@
+#ifndef menu_h_
+#define menu_h_
+void run_menu(SDL_Renderer* renderer,TTF_Font* font,SDL_Color textColor);
+void draw_time(SDL_Renderer* renderer,TTF_Font* font,SDL_Color textColor, Uint32 frametime);
+void draw_score(SDL_Renderer* renderer,TTF_Font* font,SDL_Color textColor, int score);
+void pause(SDL_Renderer* renderer,int &nani);
+void option(SDL_Renderer* renderer);
+void printText(SDL_Renderer* renderer,std::string text,int x, int y,TTF_Font* font,SDL_Color textColor);
+class dame
+{
+public:
+    static double e_dame;
+    static int e_step;
+    static int lv;
+};
+#endif menu_h_h;
